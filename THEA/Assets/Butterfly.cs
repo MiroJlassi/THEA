ecaaa986f1a3bd135e6b43b4f358d717774edcbd
Marwen <106754847+MiroJlassi@users.noreply.github.com>
2022-06-21 19:57:42 +0100

    THEA 0.0.1
    creating the project

---
+++ THEA/Assets/Butterfly.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    public Vector3 newpos;
+
+    public float timer;
+    public float timerSpeed;
+    public float timeToMove;
+    void Start()
+    {
+        newpos = new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), 0);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime * timerSpeed;
+
+        if (timer >= timeToMove)
+        {
+            transform.position = Vector3.Lerp(transform.position, newpos, Time.deltaTime * speed);
+
+            if (Vector3.Distance(transform.position,newpos) <= 0.5f)
+            {
+                newpos = new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), 0);
+
+                timer = 0.0f;
+            }
+        }
+    }
+}
---
+++ THEA/Assets/player_mvt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_mvt : MonoBehaviour
+{
+    public player_controller controller;
+
+    float horizmove = 0f;
+    public float speed;
+
+
+    bool jump = false;
+    // Start is called before the first frame update
+    void Update()
+    {
+        horizmove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        controller.Move(horizmove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
